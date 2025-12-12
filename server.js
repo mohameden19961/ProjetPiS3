05f0f3ff174863234ad85b5037fc2cad630fd2c5
abdy <24068@supnum.mr>
2025-12-12 10:43:25 +0000

    une modufication sur server.js

--- server.js
+++ server.js
@@ -2,6 +2,5 @@ const app = require('./src/app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Serveur d'examen démarré sur http://localhost:${PORT}`);
-    console.log(`Accessible aux étudiants sur l'IP locale du professeur.`);
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
